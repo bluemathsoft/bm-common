25c6d94781ede39334ba749a7a59d9f8e7777952
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-09-18 08:26:24 -0400

    Change license to Apache 2.0

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+/*
+
+Copyright (C) 2017 Jayesh Salvi, Blue Math Software Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
 
 export type NumberType = 'i8'|'ui8'|'i16'|'ui16'|'i32'|'ui32'|'f32'|'f64';
 export type TypedArray = Int8Array | Uint8Array | Int16Array |
